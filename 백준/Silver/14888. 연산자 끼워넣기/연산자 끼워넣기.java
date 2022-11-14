7ab9222af090066a22dcea5e21e5adc8e9e64f94
Hyeonjin Heo <72252806+heohyeonjin@users.noreply.github.com>
2022-11-14 22:11:41 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 132 ms, Memory: 15708 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,11 +1,10 @@
 
-// 8:34
-// 재귀
 import java.util.*;
 import java.io.*;
 public class Main {
     static int tmpOperator[];
     static int operand[];
+    static int operator[];
     static int minAnswer = Integer.MAX_VALUE;
     static int maxAnswer = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException{
@@ -17,68 +16,35 @@ public class Main {
             operand[i] = Integer.parseInt(input[i]);
         }
         input = br.readLine().split(" ");
-        int operator[] = new int[input.length];
+        operator = new int[input.length];
         for(int i=0;i<input.length;i++){
             operator[i] = Integer.parseInt(input[i]);
         }
         tmpOperator = new int[operandNum-1];
-        permu(0,operandNum,operator);
+        dfs(1,operandNum,operand[0]);
 
         System.out.println(maxAnswer);
         System.out.println(minAnswer);
     }
 
-    public static void permu(int depth, int n, int[] operator){
-        if(depth==n-1){
-            evalute(tmpOperator);
+    public static void dfs(int depth, int n, int num){
+        if(depth==n){
+            maxAnswer = Math.max(num,maxAnswer);
+            minAnswer = Math.min(num,minAnswer);
             return;
         }
-        int len = operator.length;
-        int memOperator[] = new int[len];
-        for(int i=0;i<len;i++){
-            memOperator[i] = operator[i];
-        }
-        for(int i=0;i<len;i++){
+
+        for(int i=0;i<operator.length;i++){
             if(operator[i]>0){
-                tmpOperator[depth] = i;
                 operator[i]--;
-                permu(depth+1, n, operator);
-
-                for(int j=0;j<len;j++){
-                    operator[j] = memOperator[j];
+                switch(i){
+                    case 0 : dfs(depth+1,n,num+operand[depth]); break;
+                    case 1 : dfs(depth+1, n, num-operand[depth]); break;
+                    case 2 : dfs(depth+1,n,num*operand[depth]); break;
+                    case 3 : dfs(depth+1,n,num/operand[depth]); break;
                 }
+                operator[i]++;
             }
-        }
-    }
-
-    public static void evalute(int tmpOperator[]){
-        int tmp = operand[0];
-        for(int i=1;i<operand.length;i++){
-            if(tmpOperator[i-1]==0){ // 덧셈
-                tmp+=operand[i];
-            }
-            else if(tmpOperator[i-1]==1){ // 뺄셈
-                tmp-=operand[i];
-            }
-            else if(tmpOperator[i-1]==2){ // 곱셈
-                tmp*=operand[i];
-            }
-            else{ // 나눗셈
-                if(tmp<0){
-                    tmp = Math.abs(tmp);
-                    tmp/=operand[i];
-                    tmp*=-1;
-                }
-                else{
-                    tmp/=operand[i];
-                }
-            }
-        }
-        if(tmp<minAnswer){
-            minAnswer = tmp;
-        }
-        if(tmp>maxAnswer){
-            maxAnswer = tmp;
         }
     }
 
