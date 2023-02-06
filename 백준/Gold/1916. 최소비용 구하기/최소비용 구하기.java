11d3195199fa348ab32e46428e06fbd28bb0114f
Hyeonjin Heo <72252806+heohyeonjin@users.noreply.github.com>
2023-02-06 15:26:41 +0900

    [Gold V] Title: 최소비용 구하기, Time: 600 ms, Memory: 61520 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+class Country implements Comparable<Country>{
+    public int v;
+    public int cost;
+
+    public Country(int v, int cost){
+        this.v = v;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Country o){
+        if(this.cost==o.cost){
+            return this.v-o.v;
+        }
+        return this.cost-o.cost;
+    }
+}
+public class Main {
+    static ArrayList<Country> graph[];
+    static int dp[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            String input[] = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            int cost = Integer.parseInt(input[2]);
+
+            graph[a].add(new Country(b, cost));
+        }
+
+        dp = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        String input[] = br.readLine().split(" ");
+        int from = Integer.parseInt(input[0]);
+        int to = Integer.parseInt(input[1]);
+        System.out.println(BFS(from,to,0));
+    }
+
+    public static int BFS(int v, int des, int cost) {
+        PriorityQueue<Country> pq = new PriorityQueue<>();
+        pq.offer(new Country(v,cost));
+
+        while(!pq.isEmpty()){
+            Country cur = pq.poll();
+            if(cur.v==des){
+                return cur.cost;
+            }
+            for(Country x : graph[cur.v]){
+                if(cur.cost+x.cost<dp[x.v]){
+                    dp[x.v] = cur.cost+x.cost;
+                    pq.offer(new Country(x.v,cur.cost+x.cost));
+                }
+
+            }
+        }
+        return cost;
+    }
+}
