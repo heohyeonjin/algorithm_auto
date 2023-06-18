78bb50ba87dfcbafe134d560fb82a7cfb2baa73e
Hyeonjin Heo <72252806+heohyeonjin@users.noreply.github.com>
2023-06-18 15:38:16 +0900

    [Silver II] Title: 좌표 압축, Time: 2376 ms, Memory: 265700 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sortednums[] = new int[N];
+        int nums[] = new int[N];
+        for(int i=0;i<N;i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+            sortednums[i] = nums[i];
+        }
+
+        Arrays.sort(sortednums);
+        HashMap<Integer, Integer> orderMap = new HashMap<>();
+
+        int order = 0;
+        for(int i=0;i<N;i++){
+            if(!orderMap.containsKey(sortednums[i])){
+                orderMap.put(sortednums[i],order);
+                order++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<N;i++){
+            sb.append(orderMap.get(nums[i]));
+            sb.append(" ");
+        }
+        System.out.println(sb);
+    }
+}
