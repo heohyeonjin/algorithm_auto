9d7d14d53392a0e218fd8134258ad085c761eafe
Hyeonjin Heo <72252806+heohyeonjin@users.noreply.github.com>
2023-06-20 23:01:53 +0900

    [Gold V] Title: 입국심사, Time: 512 ms, Memory: 28620 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -1,64 +1,41 @@
-import java.util.*;
 import java.io.*;
-class Main{
-        static int N,M;
-        static long simsa[];
-        public static void main(String[] args) throws IOException{
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                String input[] = br.readLine().split(" ");
-                N = Integer.parseInt(input[0]);
-                M = Integer.parseInt(input[1]);
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input[] = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
 
-                simsa = new long[N];
-                for(int i=0;i<N;i++){
-                        simsa[i] = Integer.parseInt(br.readLine());
-                }
-                Arrays.sort(simsa);
-                long answer = BinarySearch(simsa[0], simsa[N-1]*M);
-                System.out.println(answer);
+        long T[] = new long[N];
+        for(int i=0;i<N;i++){
+            T[i] = Long.parseLong(br.readLine());
         }
+        Arrays.sort(T);
 
-        public static long BinarySearch(long left, long right){
-                long answer = 0;
-                while(left<=right){
-                        long mid = (left+right)/2; // 걸리는 시간
-                        long count = CanSimsa(mid);
-                        if(count<M){
-                                left = mid+1;
-                        }
-                        else{
-                                answer = mid;
-                                right = mid-1;
-                        }
-                }
-                return answer;
+        long answer = binarySearch(1,T[0]*M,T,M);
+        System.out.println(answer);
+    }
+    public static long binarySearch(long left, long right, long T[], int M){
+        long result = Long.MAX_VALUE;
+
+        while(left<=right){
+            long sum = 0;
+            long mid = (left+right)/2;
+
+            for(long x : T){
+                sum+=mid/x;
+            }
+
+            if(sum>=M){
+                result = Math.min(result, mid);
+                right = mid-1;
+            }
+
+            else{
+                left = mid+1;
+            }
         }
-
-        public static long CanSimsa(long time){
-                long count = 0;
-                for(int i=0;i<N;i++){
-                        long t = simsa[i]; // 심사대 시간
-                        count+=time/t;
-                        if(count>M) break;
-                }
-                return count;
-        }
-
-//        // 입국 가능한 사람 수 구하기
-//        public static long count_num(long sinsa_time, long total_time){
-//                int left = 0;
-//                int right = M;
-//                int answer = 0;
-//                while(left<=right){
-//                        int mid = (left+right)/2;
-//                        if(mid*sinsa_time>total_time){
-//                                right = mid-1;
-//                        }
-//                        else{
-//                                answer = mid;
-//                                left = mid+1;
-//                        }
-//                }
-//                return answer;
-//        }
+        return result;
+    }
 }
